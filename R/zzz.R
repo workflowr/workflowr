ed925bbb4f3352455bcfe1642f528855a22af525
John Blischak <jdblischak@gmail.com>
2016-12-20 10:31:32 -0600

    Add rmarkdown to Depends so that it is loaded with workflowr. Also add packageStartupMessage.

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+# Resources:
+# .onAttach & zzz.R explained: http://r-pkgs.had.co.nz/r.html#r-differences
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(sprintf("This is workflowr version %s",
+                                packageVersion("workflowr")))
+  packageStartupMessage("Please send bug reports and feature requests to:")
+  packageStartupMessage("https://github.com/jdblischak/workflowr/issues")
+}

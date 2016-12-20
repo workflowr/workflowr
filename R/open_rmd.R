f6e572838a00af0f519ec91f01de6d354d84c0a8
John Blischak <jdblischak@gmail.com>
2016-12-20 15:21:17 -0600

    Add open_rmd function to expedite the start of writing code.

---
+++ R/open_rmd.R
@@ -0,0 +1,69 @@
+#' Open an R Markdown analysis file
+#'
+#' \code{open_rmd} opens an R Markdown file in RStudio and sets the working
+#' directory to \code{analysis/}. If the file does not exist, it creates a new
+#' Rmd using the workflowr template.
+#'
+#' \code{open_rmd} is a convenience function to make it easier to start working,
+#' especially when starting a new analysis. It will create the new file if
+#' necessary, set the working directory to \code{analysis/}, and open the file
+#' in RStudio. The latter two side effects can be turned off if desired.
+#'
+#' If you are not using RStudio and are comfortable with relative paths and
+#' managing your working directory, you could instead use directly use the
+#' underlying commands. First set the working directory to the \code{analysis/}
+#' subdirectory of your workflowr project. Second create the new file with
+#' \code{draft("model-data.Rmd", template = "analysis", package = "workflowr",
+#' edit = FALSE)}. Third open the file in your text editor.
+#'
+#' @param filename The basename of the R Markdown file.
+#' @param change_wd Should the working directory be changed to the analysis
+#'   subdirectory? (default: TRUE)
+#' @param open_file Should the file be opened in RStudio editor? (default: TRUE)
+#' @param path By default the function assumes the current working directory is
+#'   within the project. If this is not true, you'll need to provide the path to
+#'   the project directory.
+#'
+#' @return Invisibly returns the full path to the R Markdown file.
+#'
+#' @examples
+#' \dontrun{
+#' open_rmd("model-data.Rmd")
+#' }
+#' @export
+open_rmd <- function(filename,
+                     change_wd = TRUE,
+                     open_file = TRUE,
+                     path = ".") {
+  filename_base <- basename(filename)
+  if (filename != filename_base) {
+    message("filename should just be the basename.")
+    message(sprintf("e.g. %s", filename_base))
+    message(sprintf("not %s", filename))
+    message("It will be saved in analysis/")
+    stop(sprintf("open_rmd: Invalid filename argument"))
+  }
+  analysis_dir <- rprojroot::find_rstudio_root_file("analysis", path = path)
+  rmd_filename <- file.path(analysis_dir, filename)
+  # Create the file if it doesn't exist
+  if (!file.exists(rmd_filename)) {
+    rmarkdown::draft(rmd_filename, template = "analysis", package = "workflowr",
+                     edit = FALSE)
+  }
+  # Change the working directory
+  current_wd <- getwd()
+  if (change_wd & current_wd == analysis_dir) {
+    message("Current working directory not changed. Already set to:")
+    message(analysis_dir)
+  } else if (change_wd) {
+    setwd(analysis_dir)
+    message("Current working directory changed to:")
+    message(analysis_dir)
+  }
+  # Open in RStudio
+  if (rstudioapi::isAvailable() & open_file) {
+    rstudioapi::navigateToFile(rmd_filename)
+  }
+
+  return(invisible(rmd_filename))
+}
---
+++ tests/testthat/test-open.R
@@ -0,0 +1,71 @@
+# These tests cannot be run automatically with `devtools::test`. Instead of
+# locating the workflowr package installed in my local library, it locates the
+# source code I am trying to test. This breaks it, because the directory
+# structure is different (the inst/ is removed when the package is installed). I
+# suspect this has to do with devtools having its own `system.file` to override
+# `base::system.file` (see the link to pkgload package below). The function
+# `rmarkdown::draft` calls `system.file`, so there is no way for me to prevent
+# this behavior. This is really frustrating. However, it can be tested manually
+# using the following steps.
+
+# https://github.com/r-pkgs/pkgload/blob/master/R/shims.r#L8
+
+# I commented on an open issue on devtools about system.file causing problems
+# with testthat.
+# https://github.com/hadley/devtools/issues/1404#issuecomment-268361994
+
+# 1. Restart R session (even if you haven't loaded devtools, running one of its
+#    exported functions, e.g. `devtools::test`, pollutes the search path with
+#    devtools_shims)
+# 2. Change the variable `skipping` below from `TRUE` to `FALSE`
+# 3. Source this file, test-open.R
+
+skipping <- TRUE
+
+if (!skipping) {
+  library("testthat")
+  library("workflowr")
+}
+
+context("open")
+
+# start project in a tempdir
+site_dir <- tempfile()
+dir.create(site_dir)
+suppressMessages(start_project("Test open_rmd", site_dir))
+
+test_that("open_rmd creates a new file, but does not overwrite", {
+
+  if (skipping)
+    skip("Must be run manually.")
+
+  full_file_path <- open_rmd("test.Rmd", change_wd = FALSE, open_file = FALSE,
+                             path = site_dir)
+  expect_true(file.exists(full_file_path))
+  modification_time_pre <- file.mtime(full_file_path)
+  full_file_path_2 <- open_rmd("test.Rmd", change_wd = FALSE, open_file = FALSE,
+                               path = site_dir)
+  expect_true(full_file_path_2 == full_file_path)
+  modification_time_post <- file.mtime(full_file_path)
+  expect_true(modification_time_post == modification_time_pre)
+})
+
+test_that("open_rmd changes the working directory", {
+
+  if (skipping)
+    skip("Must be run manually.")
+
+  wd_pre <- getwd()
+  open_rmd("do-not-change-wd.Rmd", change_wd = FALSE, open_file = FALSE,
+           path = site_dir)
+  wd_same <- getwd()
+  expect_identical(wd_pre, wd_same)
+  open_rmd("do-change-wd.Rmd", change_wd = TRUE, open_file = FALSE,
+           path = site_dir)
+  wd_change <- getwd()
+  expect_identical(wd_change, file.path(site_dir, "analysis"))
+  # Cleanup
+  setwd(wd_pre)
+})
+
+unlink(site_dir, recursive = TRUE)

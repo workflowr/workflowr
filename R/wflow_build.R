367c09045b6b081aef1f83fcb65fbd062aff1eda
John Blischak <jdblischak@gmail.com>
2017-01-13 17:15:13 -0600

    Update wflow_build.
    * Ignore files starting with underscores
    * Option to render all files like render_site
    * More documentation and messaging

--- R/wflow_build.R
+++ R/wflow_build.R
@@ -1,16 +1,27 @@
-#' Render only the updated R Markdown files
+#' Build the website
 #'
-#' \code{wflow_build} provides Makefile-like functionality to render only the R
-#' Markdown files that have been modified. This is in contrast to
-#' \code{rmarkdown::render_site}, which always renders all the pages. You should
-#' use the latter if you've made aesthetic changes that you want applied to all
-#' the R Markdown files.
+#' \code{wflow_build} builds the website by rendering the R Markdown files in
+#' the analysis directory and saving them to \code{docs/}. By default it only
+#' renders the R Markdown files that have been modified more recently than their
+#' corresponding HTML file (similar to a Makefile). To render specific R
+#' Markdown files, pass them as a vector to the argument \code{files}. To render
+#' every single page (e.g. to change the theme across the entire site), set
+#' \code{files = "all"}.
 #'
 #' Under the hood, this runs \code{rmarkdown::render_site} on each updated file
-#' individually. This provides all the website styling (which you would lose if
-#' you ran \code{rmarkdown::render}) without re-building the entire site (which
-#' would happen if you ran \code{rmarkdown::render_site with no arguments}).
+#' individually. This provides all the website styling specified in
+#' \code{_site.yml} (which you would lose if you ran \code{rmarkdown::render})
+#' without re-building the entire site (which would happen if you ran
+#' \code{rmarkdown::render_site with no arguments}).
 #'
+#' To include R Markdown files in your workflowr project that are not included
+#' as part of the website, you have multiple options: 1) Prepend an underscore
+#' to the filename, 2) move them to a subdirectory within the analysis
+#' directory, or 3) move them to another directory at the root of your project.
+#'
+#' @param files R Markdown files to be rendered. The files can be specified
+#'   using the path or just the basename. Set to "all" to render every R
+#'   Markdown file (default: "").
 #' @param dry_run Identifies R Markdown files that have been updated, but does
 #'   not render them.
 #' @param path By default the function assumes the current working directory is
@@ -26,17 +37,64 @@
 #'
 #' @examples
 #' \dontrun{
+#' # View the files that would be rendered
+#' wflow_build(dry_run = TRUE)
+#' # Render all modified files
 #' wflow_build()
+#' # Render specific files
+#' wflow_build(files = c("one.Rmd", "two.Rmd"))
+#' # Render all files
+#' wflow_build(files = "all")
 #' }
 #' @export
-wflow_build <- function(dry_run = FALSE, path = ".", ...) {
+wflow_build <- function(files = "", dry_run = FALSE, path = ".", ...) {
+  stopifnot(is.character(files),
+            is.logical(dry_run),
+            is.character(path))
   analysis_dir <- rprojroot::find_rstudio_root_file("analysis", path = path)
   stopifnot(dir.exists(analysis_dir))
 
-  # Gather Rmd files
-  rmd_files <- Sys.glob(file.path(analysis_dir, "*Rmd"))
+  if (files == "" | files == "all") {
+    # Gather Rmd files (any file starting with _ is ignored)
+    rmd_files <- list.files(path = analysis_dir, pattern = "^[^_].*Rmd$",
+                            full.names = TRUE)
+  } else {
+    rmd_files <- file.path(analysis_dir, basename(files))
+    stopifnot(file.exists(rmd_files), grep("Rmd$", rmd_files))
+  }
+
+  if (files != "" | files == "all") {
+    files_to_update <- rmd_files
+  } else {
+    files_to_update <- return_modified_rmd(rmd_files)
+  }
+
+  if (length(files_to_update) == 0) {
+    message("All HTML files have been rendered")
+  }
+
+  # Render the updated R Markdown files
+  if (dry_run) {
+    message("The following R Markdown files would be rendered:")
+    return(files_to_update)
+  } else {
+    for (f in files_to_update) {
+      cat(sprintf("\n\nRendering %s\n\n", f))
+      rmarkdown::render_site(f, ...)
+    }
+  }
+
+  return(invisible(files_to_update))
+}
+
+# Return the R Markdown files which have been modified more recently than their
+# corresponding HTML files in docs/.
+#
+# Input: character. path to R Markdown files in analysis directory
+return_modified_rmd <- function(rmd_files) {
+
   # Expected html files
-  html_files <- stringr::str_replace(rmd_files, "Rmd", "html")
+  html_files <- stringr::str_replace(rmd_files, "Rmd$", "html")
   html_files <- stringr::str_replace(html_files, "/analysis/", "/docs/")
 
   # Determine which R Markdown files have been updated and need to be rendered
@@ -51,19 +109,5 @@ wflow_build <- function(dry_run = FALSE, path = ".", ...) {
     }
   }
 
-  if (length(files_to_update) == 0) {
-    message("All HTML files have been rendered")
-  }
-
-  # Render the updated R Markdown files
-  if (dry_run) {
-    return(files_to_update)
-  } else {
-    for (f in files_to_update) {
-      cat(sprintf("\n\nRendering %s\n\n", f))
-      rmarkdown::render_site(f, ...)
-    }
-  }
-
-  return(invisible(files_to_update))
+  return(files_to_update)
 }

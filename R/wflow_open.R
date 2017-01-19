46c53e2ee986e9535bd4ff0d6125d9be34fb44ee
John Blischak <jdblischak@gmail.com>
2017-01-19 15:33:40 -0600

    Enable wflow_open to accept multiple filenames, which can be either the basename or a path.

--- R/wflow_open.R
+++ R/wflow_open.R
@@ -1,22 +1,24 @@
-#' Open an R Markdown analysis file
+#' Open R Markdown analysis file(s)
 #'
-#' \code{wflow_open} opens an R Markdown file in RStudio and sets the working
-#' directory to \code{analysis/}. If the file does not exist, it creates a new
-#' Rmd using the workflowr template.
+#' \code{wflow_open} opens R Markdown files in RStudio and sets the working
+#' directory to \code{analysis/}. If a file does not exist, it is created using
+#' the workflowr R Markdown template.
 #'
-#' \code{wflow_open} is a convenience function to make it easier to start working,
-#' especially when starting a new analysis. It will create the new file if
-#' necessary, set the working directory to \code{analysis/}, and open the file
-#' in RStudio. The latter two side effects can be turned off if desired.
+#' \code{wflow_open} is a convenience function to make it easier to start
+#' working, especially when starting a new analysis. It will create a new file
+#' if necessary, set the working directory to \code{analysis/}, and open the
+#' file(s) in RStudio. The latter two side effects can be turned off if desired.
 #'
 #' If you are not using RStudio and are comfortable with relative paths and
-#' managing your working directory, you could instead use directly use the
+#' managing your working directory, you could instead directly use the
 #' underlying commands. First set the working directory to the \code{analysis/}
 #' subdirectory of your workflowr project. Second create the new file with
 #' \code{draft("model-data.Rmd", template = "analysis", package = "workflowr",
 #' edit = FALSE)}. Third open the file in your text editor.
 #'
-#' @param filename The basename of the R Markdown file.
+#' @param filename The name of the R Markdown file(s) (can be either a path or
+#'   just the basename, but will always be written to \code{analysis/}
+#'   directory). File extension must be .Rmd.
 #' @param change_wd Should the working directory be changed to the analysis
 #'   subdirectory? (default: TRUE)
 #' @param open_file Should the file be opened in RStudio editor? (default: TRUE)
@@ -24,33 +26,53 @@
 #'   within the project. If this is not true, you'll need to provide the path to
 #'   the project directory.
 #'
-#' @return Invisibly returns the full path to the R Markdown file.
+#' @return Invisibly returns the full path to the R Markdown file(s).
 #'
 #' @examples
 #' \dontrun{
+#' # Any of the following are valid input. Files would be created if they did
+#' # not exist.
+#' # Basename of file:
 #' wflow_open("model-data.Rmd")
+#' # Path to file
+#' wflow_open("analysis/model-data.Rmd")
+#' # Multiple files
+#' wflow_open(c("model-data.Rmd", "another-analysis.Rmd"))
 #' }
 #' @import rmarkdown
 #' @export
 wflow_open <- function(filename,
-                     change_wd = TRUE,
-                     open_file = TRUE,
-                     path = ".") {
-  filename_base <- basename(filename)
-  if (filename != filename_base) {
-    message("filename should just be the basename.")
-    message(sprintf("e.g. %s", filename_base))
-    message(sprintf("not %s", filename))
-    message("It will be saved in analysis/")
-    stop(sprintf("wflow_open: Invalid filename argument"))
-  }
+                       change_wd = TRUE,
+                       open_file = TRUE,
+                       path = ".") {
+  stopifnot(is.character(filename),
+            grepl("Rmd$", filename),
+            is.logical(change_wd),
+            is.logical(open_file),
+            is.character(path))
   analysis_dir <- rprojroot::find_rstudio_root_file("analysis", path = path)
-  rmd_filename <- file.path(analysis_dir, filename)
-  # Create the file if it doesn't exist
-  if (!file.exists(rmd_filename)) {
-    rmarkdown::draft(rmd_filename, template = "analysis", package = "workflowr",
-                     edit = FALSE)
+  stopifnot(dir.exists(analysis_dir))
+  rmd_filename <- file.path(analysis_dir, basename(filename))
+
+  # Provide warning if user provides a path to a directory other than analysis
+  rmd_dirname <- dirname(filename)
+  rmd_path <- stringr::str_split(rmd_dirname, .Platform$file.sep)
+  rmd_subdir <- sapply(rmd_path, function(x) x[length(x)])
+  for (i in seq_along(rmd_filename)) {
+    if (!(rmd_subdir[i] == "analysis" | rmd_subdir[i] == ".")) {
+      warning(sprintf("Input file had invalid subdirectory specified.\nInput filename: %s\nOutput filename: %s",
+                      filename[i], rmd_filename[i]))
+    }
   }
+
+  # Create the file(s) if non-existent
+  for (rmd in rmd_filename) {
+    if (!file.exists(rmd)) {
+      rmarkdown::draft(rmd, template = "analysis", package = "workflowr",
+                       edit = FALSE)
+    }
+  }
+
   # Change the working directory
   current_wd <- getwd()
   if (change_wd & current_wd == analysis_dir) {
@@ -63,7 +85,9 @@ wflow_open <- function(filename,
   }
   # Open in RStudio
   if (rstudioapi::isAvailable() & open_file) {
-    rstudioapi::navigateToFile(rmd_filename)
+    for (rmd in rmd_filename) {
+      rstudioapi::navigateToFile(rmd)
+    }
   }
 
   return(invisible(rmd_filename))
--- tests/testthat/test-open.R
+++ tests/testthat/test-open.R
@@ -1,10 +1,10 @@
 # These tests cannot be run automatically with `devtools::test`. Instead of
-# locating the workflowr package installed in my local library, it locates the
-# source code I am trying to test. This breaks it, because the directory
+# locating the workflowr package installed in the local library, it locates the
+# source code being tested. This breaks it, because the directory
 # structure is different (the inst/ is removed when the package is installed). I
 # suspect this has to do with devtools having its own `system.file` to override
 # `base::system.file` (see the link to pkgload package below). The function
-# `rmarkdown::draft` calls `system.file`, so there is no way for me to prevent
+# `rmarkdown::draft` calls `system.file`, so there is no clear way to prevent
 # this behavior. This is really frustrating. However, it can be tested manually
 # using the following steps.
 
@@ -17,10 +17,14 @@
 # 1. Restart R session (even if you haven't loaded devtools, running one of its
 #    exported functions, e.g. `devtools::test`, pollutes the search path with
 #    devtools_shims)
-# 2. Change the variable `skipping` below from `TRUE` to `FALSE`
-# 3. Source this file, test-open.R
+# 2. Source this file, test-open.R
 
-skipping <- TRUE
+if ("devtools_shims" %in% search()) {
+  skipping <- TRUE
+} else {
+  skipping <- FALSE
+  message("Running tests in test-open.R")
+}
 
 if (!skipping) {
   library("testthat")
@@ -45,9 +49,9 @@ test_that("wflow_open creates a new file, but does not overwrite", {
   modification_time_pre <- file.mtime(full_file_path)
   full_file_path_2 <- wflow_open("test.Rmd", change_wd = FALSE, open_file = FALSE,
                                path = site_dir)
-  expect_true(full_file_path_2 == full_file_path)
+  expect_identical(full_file_path_2, full_file_path)
   modification_time_post <- file.mtime(full_file_path)
-  expect_true(modification_time_post == modification_time_pre)
+  expect_identical(modification_time_post, modification_time_pre)
 })
 
 test_that("wflow_open changes the working directory", {
@@ -66,6 +70,52 @@ test_that("wflow_open changes the working directory", {
   expect_identical(wd_change, file.path(site_dir, "analysis"))
   # Cleanup
   setwd(wd_pre)
+})
+
+test_that("wflow_open can accept multiple files", {
+
+  if (skipping)
+    skip("Must be run manually.")
+  rmd_multi <- paste0(1:3, ".Rmd")
+  full_file_path <- wflow_open(rmd_multi, change_wd = FALSE, open_file = FALSE,
+                               path = site_dir)
+  expect_true(all(file.exists(full_file_path)))
+  modification_time_pre <- file.mtime(full_file_path)
+  full_file_path_2 <- wflow_open(rmd_multi, change_wd = FALSE, open_file = FALSE,
+                                 path = site_dir)
+  expect_identical(full_file_path_2, full_file_path)
+  modification_time_post <- file.mtime(full_file_path)
+  expect_identical(modification_time_post, modification_time_pre)
+})
+
+test_that("wflow_open can accept basename, full paths, and wrong paths", {
+
+  if (skipping)
+    skip("Must be run manually.")
+  rmd_paths <- c("basename.Rmd",
+                 file.path(site_dir, "analysis", "full.Rmd"),
+                 file.path(site_dir, "code", "wrong.Rmd"))
+  expect_warning(full_file_path <-
+                   wflow_open(rmd_paths, change_wd = FALSE, open_file = FALSE,
+                               path = site_dir),
+                 "Input file had invalid subdirectory specified.")
+  expect_true(all(file.exists(full_file_path)))
+  modification_time_pre <- file.mtime(full_file_path)
+  full_file_path_2 <- wflow_open(rmd_paths, change_wd = FALSE, open_file = FALSE,
+                                 path = site_dir)
+  expect_identical(full_file_path_2, full_file_path)
+  modification_time_post <- file.mtime(full_file_path)
+  expect_identical(modification_time_post, modification_time_pre)
+})
+
+test_that("wflow_open rejects filenames without Rmd extension", {
+
+  if (skipping)
+    skip("Must be run manually.")
+
+  expect_error(wflow_open("file.rmd", change_wd = FALSE, open_file = FALSE,
+                          path = site_dir),
+               "grepl")
 })
 
 unlink(site_dir, recursive = TRUE)

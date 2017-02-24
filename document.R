a84755fa64a34e27f4c9ddee441ce9df9f98d235
John Blischak <jdblischak@gmail.com>
2017-02-24 15:43:04 -0600

    Add convenience R script for documenting package.

---
+++ document.R
@@ -0,0 +1,16 @@
+#!/usr/bin/env Rscript
+
+# This is a convenience script for when I'm developing the package. The main
+# issue is that building the package in RStudio (Ctrl+Shift+B) or documenting
+# the package (Ctrl+Shift+D) builds the vignettes and pollutes `vignettes/` with
+# the generated `*R` and `*html` files instead of putting them in `inst/doc/`.
+# This script ensures the documentation is up-to-date and also removes
+# unnecessary files.
+
+stopifnot(file.exists("workflowr.Rproj"))
+
+devtools::document()
+devtools::clean_vignettes(); devtools::build_vignettes(); devtools::clean_vignettes()
+
+unlink("docs/articles/*R")
+pkgdown::build_site()

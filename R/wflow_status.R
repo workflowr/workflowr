508952f85103952aa89f7280ec06bbe665c0bacf
John Blischak <jdblischak@gmail.com>
2017-04-24 17:13:39 -0500

    Add documentation for wflow_status and make it easier to copy-paste directory from the console output.

--- R/wflow_status.R
+++ R/wflow_status.R
@@ -2,6 +2,49 @@
 #'
 #' \code{wflow_status} reports the current status of the workflowr project.
 #'
+#' \code{wflow_status} reports the paths to the main directories of the
+#' workflowr project:
+#'
+#' \itemize{
+#'
+#' \item \bold{working directory}: The current directory in the R console (i.e.
+#' \code{getwd{}}).
+#'
+#' \item \bold{project root}: The directory that contains the RStudio .Rproj
+#' file.
+#'
+#' \item \bold{analysis}: The directory that contains \code{_site.yml} and the R
+#' Markdown files.
+#'
+#' \item \bold{docs}: The directory that contains the HTML files and figures.
+#'
+#' \item \bold{git}: The \code{.git} directory that contains the history of the
+#' Git repository.
+#'
+#' }
+#'
+#' \code{wflow_status} also reports the status of files that require user
+#' action:
+#'
+#' \itemize{
+#'
+#' \item \bold{outdated}: When an R Markdown file has been committed to the
+#' repository without updating the previously published HTML file.
+#'
+#' \item \bold{staged}: When an R Markdown file has changes that have been added
+#' to the index (e.g. with \code{git add}).
+#'
+#' \item \bold{unstaged}: When a tracked R Markdown file has changes in the
+#' working directory.
+#'
+#' \item \bold{untracked}: When an R Markdown file has not been added or
+#' committed to the Git repository.
+#'
+#' \item \bold{ignored}: When an R Markdown file has been ignored by Git
+#' according to the patterns in the file \code{.gitignore}.
+#'
+#' }
+#'
 #' @param verbose logical (default: TRUE). Prints status to R console.
 #' @param project character (default: ".") By default the function assumes the
 #'   current working directory is within the project. If this is not true,
@@ -126,48 +169,48 @@ print.wflow_status <- function(x, ...) {
 
 You have the following directories set:
 
-working:      {{wd}}
-project root: {{root}}
-analysis:     {{analysis}}
-docs:         {{docs}}
-git:          {{git}}
+working:      \"{{{wd}}}\"
+project root: \"{{root}}\"
+analysis:     \"{{analysis}}\"
+docs:         \"{{docs}}\"
+git:          \"{{git}}\"
 
 You have {{n_analysis}} R Markdown files ({{n_published}} published).
 
 {{#display_outdated}}
   * {{n_outdated}} outdated.
 
-{{f_outdated}}
+{{{f_outdated}}}
 
 {{/display_outdated}}
 {{#display_unpublished}}
   * {{n_unpublished}} unpublished.
 
-{{f_unpublished}}
+{{{f_unpublished}}}
 
 {{/display_unpublished}}
 {{#display_staged}}
   * {{n_staged}} with staged changes.
 
-{{f_staged}}
+{{{f_staged}}}
 
 {{/display_staged}}
 {{#display_unstaged}}
   * {{n_unstaged}} with unstaged changes.
 
-{{f_unstaged}}
+{{{f_unstaged}}}
 
 {{/display_unstaged}}
 {{#display_untracked}}
   * {{n_untracked}} untracked by Git.
 
-{{f_untracked}}
+{{{f_untracked}}}
 
 {{/display_untracked}}
 {{#display_ignored}}
   * {{n_ignored}} ignored by Git.
 
-{{f_ignored}}
+{{{f_ignored}}}
 
 {{/display_ignored}}
 
@@ -196,28 +239,38 @@ file to the Git repository without yet publishing the HTML, use
 
     display_outdated = if (any(outdated)) TRUE else FALSE,
     n_outdated = if (any(outdated)) sum(outdated) else NA,
-    f_outdated = if (any(outdated)) paste(basename(x$files[outdated]), collapse = "\n") else NA,
+    f_outdated = if (any(outdated)) display_files(x$files[outdated]) else NA,
 
     display_unpublished = if (any(unpublished)) TRUE else FALSE,
     n_unpublished = if (any(unpublished)) sum(unpublished) else NA,
-    f_unpublished = if (any(unpublished)) paste(basename(x$files[unpublished]), collapse = "\n") else NA,
+    f_unpublished = if (any(unpublished)) display_files(x$files[unpublished]) else NA,
 
     display_staged = if (any(staged)) TRUE else FALSE,
     n_staged = if (any(staged)) sum(staged) else NA,
-    f_staged = if (any(staged)) paste(basename(x$files[staged]), collapse = "\n") else NA,
+    f_staged = if (any(staged)) display_files(x$files[staged]) else NA,
 
     display_unstaged = if (any(unstaged)) TRUE else FALSE,
     n_unstaged = if (any(unstaged)) sum(unstaged) else NA,
-    f_unstaged = if (any(unstaged)) paste(basename(x$files[unstaged]), collapse = "\n") else NA,
+    f_unstaged = if (any(unstaged)) display_files(x$files[unstaged]) else NA,
 
     display_untracked = if (any(untracked)) TRUE else FALSE,
     n_untracked = if (any(untracked)) sum(untracked) else NA,
-    f_untracked = if (any(untracked)) paste(basename(x$files[untracked]), collapse = "\n") else NA,
+    f_untracked = if (any(untracked)) display_files(x$files[untracked]) else NA,
 
     display_ignored = if (any(ignored)) TRUE else FALSE,
     n_ignored = if (any(ignored)) sum(ignored) else NA,
-    f_ignored = if (any(ignored)) paste(basename(x$files[ignored]), collapse = "\n") else NA)
+    f_ignored = if (any(ignored)) display_files(x$files[ignored]) else NA)
 
   m <- whisker::whisker.render(template, variables)
   cat(m)
+}
+
+display_files <- function(files) {
+  out <- "c("
+  for (f in files) {
+    out <- paste0(out, "\"", f, "\",\n")
+  }
+  out <- stringr::str_sub(out, 1, nchar(out) - 2)
+  out <- paste0(out, ")")
+  return(out)
 }

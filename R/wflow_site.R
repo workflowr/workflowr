8fcae5c0a2f4f13155b327f7da589e610106d732
John Blischak <jdblischak@gmail.com>
2017-11-17 14:24:34 -0600

    Internalize knitr options.

--- R/wflow_site.R
+++ R/wflow_site.R
@@ -1,8 +1,17 @@
 
 #' @export
 wflow_site <- function(input, encoding = getOption("encoding"), ...) {
-  # browser()
   wflow_yml <- file.path(input, "..", "_workflowr.yml")
   wflow_config <- yaml::yaml.load_file(wflow_yml)
   rmarkdown:::default_site(input, encoding, ...)
+}
+
+#' @export
+wflow_document <- function(...) {
+  x <- rmarkdown::html_document(...)
+  x$knitr$opts_knit$root.dir <- ".."
+  x$knitr$opts_chunk$comment <- NA
+  x$knitr$opts_chunk$fig.align <- "center"
+  x$knitr$opts_chunk$tidy <- FALSE
+  x
 }

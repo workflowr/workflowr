97a5366c7ca959cdb80dd2e9db6a9bea8dfe17cc
John Blischak <jdblischak@gmail.com>
2018-02-07 21:07:57 -0500

    Remove unused wflow_document in favor of setting knitr chunk options in wflow_site.

--- R/wflow_site.R
+++ R/wflow_site.R
@@ -191,13 +191,3 @@ wflow_site <- function(input, encoding = getOption("encoding"), ...) {
     clean = function() stop("Not implemented. Use wflow_remove()", call. = FALSE)
   )
 }
-
-#' @export
-wflow_document <- function(...) {
-  x <- rmarkdown::html_document(...)
-  x$knitr$opts_knit$root.dir <- ".."
-  x$knitr$opts_chunk$comment <- NA
-  x$knitr$opts_chunk$fig.align <- "center"
-  x$knitr$opts_chunk$tidy <- FALSE
-  x
-}

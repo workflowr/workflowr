65f4b63bcf42356d11fa07a318903ac55b9afedb
John Blischak <jdblischak@gmail.com>
2018-04-09 22:10:58 -0400

    More documentation for wflow_html and wflow_site.

--- R/wflow_site.R
+++ R/wflow_site.R
@@ -8,7 +8,15 @@
 #' Do not call the function \code{wflow_site} directly. Instead insert the line
 #' below directly into the YAML header of the file \code{index.Rmd}:
 #'
-#' \code{site: workflowr::wflow_site}
+#' \preformatted{
+#' ---
+#' title: "Home"
+#' site: workflowr::wflow_site
+#' output:
+#'   workflowr::wflow_html:
+#'     toc: false
+#' ---
+#' }
 #'
 #' Then you can build the website by running \code{\link[rmarkdown]{render_site}}
 #' in the R console or clicking the Knit button in RStudio.

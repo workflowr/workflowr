d7c8fe1d171281a0fa9b66be062f2167506d9135
John Blischak <jdblischak@gmail.com>
2018-04-15 22:00:56 -0400

    Use withr to temporarily move .gitconfig.
    Do not test wflow_git_config on CRAN because it modifies a user's system file.

--- tests/testthat/test-wflow_git_config.R
+++ tests/testthat/test-wflow_git_config.R
@@ -2,25 +2,44 @@ context("wflow_git_config")
 
 # Setup ------------------------------------------------------------------------
 
-# Temporarily move global .gitconfig file
-if (.Platform$OS.type == "windows") {
-  # Can't use ~ because the default on Windows is the user's Documents
-  # directory.
-  # https://cran.r-project.org/bin/windows/base/rw-FAQ.html#What-are-HOME-and-working-directories_003f
+# Functions to temporarily move global .gitconfig file
+
+library("withr")
+
+# withr "set" function
+#
+# Moves .gitconfig to .gitconfig-suffix
+remove_gitconfig <- function(suffix) {
   user_home <- workflowr:::get_home()
   config_original <- file.path(user_home, ".gitconfig")
-} else {
-  config_original <- "~/.gitconfig"
+  config_tmp <- paste0(config_original, suffix)
+  if (file.exists(config_original)) {
+    file.rename(from = config_original, to = config_tmp)
+  }
+  return(config_tmp)
 }
-if (file.exists(config_original)) {
-  config_tmp <- paste0(config_original, "-workflowr")
-  file.rename(from = config_original, to = config_tmp)
-  on.exit(file.rename(from = config_tmp, to = config_original))
+
+# withr "reset" function
+#
+# Moves .gitconfig-suffix to .gitconfig
+restore_gitconfig <- function(config_tmp) {
+  user_home <- workflowr:::get_home()
+  config_original <- file.path(user_home, ".gitconfig")
+  if (file.exists(config_tmp)) {
+    file.rename(from = config_tmp, to = config_original)
+  }
 }
+
+local_no_gitconfig <- withr::local_(set = remove_gitconfig,
+                                    reset = restore_gitconfig)
 
 # Test wflow_git_config --------------------------------------------------------
 
 test_that("wflow_git_config has no effect when run without arguments", {
+
+  skip_on_cran()
+
+  local_no_gitconfig("-workflowr")
   result <- wflow_git_config()
   git_config <- git2r::config(global = TRUE)
   expect_null(git_config$global$user.name)
@@ -33,6 +52,10 @@ test_that("wflow_git_config has no effect when run without arguments", {
 })
 
 test_that("wflow_git_config can set user.name", {
+
+  skip_on_cran()
+
+  local_no_gitconfig("-workflowr")
   result <- wflow_git_config(user.name = "A Name")
   git_config <- git2r::config(global = TRUE)
   expect_true(git_config$global$user.name == "A Name")
@@ -43,22 +66,30 @@ test_that("wflow_git_config can set user.name", {
 })
 
 test_that("wflow_git_config can set user.email", {
+
+  skip_on_cran()
+
+  local_no_gitconfig("-workflowr")
   result <- wflow_git_config(user.email = "email@domain")
   git_config <- git2r::config(global = TRUE)
   expect_true(git_config$global$user.email == "email@domain")
   expect_identical(utils::capture.output(result),
-                   c("Current Git user.name:\tA Name",
+                   c("Current Git user.name needs to be configured",
                      "Current Git user.email:\temail@domain",
                      "Other Git settings:", ""))
 })
 
 test_that("wflow_git_config can set arbitrary settings", {
+
+  skip_on_cran()
+
+  local_no_gitconfig("-workflowr")
   result <- wflow_git_config(core.editor = "nano")
   git_config <- git2r::config(global = TRUE)
   expect_true(git_config$global$core.editor == "nano")
   expect_identical(utils::capture.output(result),
-                   c("Current Git user.name:\tA Name",
-                     "Current Git user.email:\temail@domain",
+                   c("Current Git user.name needs to be configured",
+                     "Current Git user.email needs to be configured",
                      "Other Git settings:",
                      "\tcore.editor:\tnano", ""))
 })

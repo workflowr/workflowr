64e7ee3ab3b67c347534d4b5949e33f0807e47ce
John Blischak <jdblischak@gmail.com>
2019-02-10 22:34:29 -0500

    Experiment with winbuilder temp directory.

--- tests/testthat/test-winbuilder.R
+++ tests/testthat/test-winbuilder.R
@@ -2,11 +2,8 @@ context("winbuilder")
 
 # Attempting to diagnose winbuilder-specific errors
 
-test_that("fs::dir_exists returns FALSE for non-existent directories starting at root", {
-  expect_false(fs::dir_exists("/a/b/c"))
-  expect_identical(dirname("/x"), "/")
-  expect_identical(fs::path_abs(dirname("/x")), fs::path_abs("/"))
-})
+if (!(.Platform$OS.type == "windows" && Sys.getenv("USERPROFILE") == "C:\\Users\\CRAN"))
+  skip("Only relevant on winbuilder")
 
 test_that("git2r can add and commit a file", {
 
@@ -16,6 +13,7 @@ test_that("git2r can add and commit a file", {
   # Adapting the example from ?git2r::commit
   path <- tempfile(pattern = "git2r-")
   dir.create(path)
+  path <- workflowr:::absolute(path)
   repo <- git2r::init(path)
   git2r::config(repo, user.name = "Alice", user.email = "alice@example.org")
   writeLines("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do",
@@ -25,9 +23,21 @@ test_that("git2r can add and commit a file", {
   expect_identical(class(commit_1), "git_commit")
 })
 
-test_that("Obtain winbuilder environment variables", {
-  expect_identical(Sys.getenv("HOMEDRIVE"), "D:")
-  expect_identical(Sys.getenv("USERPROFILE"), "D:\\win\\user")
-  expect_identical(Sys.getenv("HOMEPATH"), "\\win\\user")
-  expect_identical(resolve_symlink("/x/y/z"), "/x/y/z")
+test_that("Obtain winbuilder info on temp directory", {
+  expect_identical(tempdir(), "What is the temporary directory path?")
+  x <- tempfile()
+  expect_identical(x, "What is an example tempfile?")
+  fs::file_create(x)
+  expect_identical(workflowr:::absolute(x), "What is an example tempfile after absolute?")
+  expect_identical(workflowr:::relative(x), "What is an example tempfile after relative?")
+  expect_identical(workflowr:::absolute(workflowr:::relative(x)), "What is an example tempfile after relative and absolute?")
+})
+
+test_that("Obtain winbuilder info on temp directory (fs)", {
+  x <- fs::file_temp()
+  expect_identical(x, "(fs) What is an example tempfile?")
+  fs::file_create(x)
+  expect_identical(fs::path_abs(x), "What is an example tempfile after path_abs?")
+  expect_identical(fs::path_rel(x), "What is an example tempfile after path_rel?")
+  expect_identical(fs::path_abs(fs::path_rel(x)), "What is an example tempfile after path_rel and path_abs?")
 })

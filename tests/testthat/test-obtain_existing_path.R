15753d1da90a3d74d0e06a0a5071a8aeae01dea8
John Blischak <jdblischak@gmail.com>
2019-02-10 22:14:10 -0500

    Make obtain_existing_path tests use OS-specific root directory.

--- tests/testthat/test-obtain_existing_path.R
+++ tests/testthat/test-obtain_existing_path.R
@@ -26,75 +26,55 @@ test_that("obtain_existing_paths works with nested non-existing directories", {
 })
 
 test_that("obtain_existing_paths works with relative path to cwd", {
-expect_identical(obtain_existing_path("./x"), workflowr:::absolute("."))
-expect_identical(obtain_existing_path("./x/"), workflowr:::absolute("."))
-expect_identical(obtain_existing_path("./x/y"), workflowr:::absolute("."))
-expect_identical(obtain_existing_path("./x/y/"), workflowr:::absolute("."))
-expect_identical(obtain_existing_path("./x/y/z"), workflowr:::absolute("."))
+  expect_identical(obtain_existing_path("./x"), workflowr:::absolute("."))
+  expect_identical(obtain_existing_path("./x/"), workflowr:::absolute("."))
+  expect_identical(obtain_existing_path("./x/y"), workflowr:::absolute("."))
+  expect_identical(obtain_existing_path("./x/y/"), workflowr:::absolute("."))
+  expect_identical(obtain_existing_path("./x/y/z"), workflowr:::absolute("."))
 })
 
 test_that("obtain_existing_paths works with relative path to upstream", {
-expect_identical(obtain_existing_path("../x"), workflowr:::absolute(".."))
-expect_identical(obtain_existing_path("../x/"), workflowr:::absolute(".."))
-expect_identical(obtain_existing_path("../x/y"), workflowr:::absolute(".."))
-expect_identical(obtain_existing_path("../x/y/"), workflowr:::absolute(".."))
-expect_identical(obtain_existing_path("../x/y/z"), workflowr:::absolute(".."))
+  expect_identical(obtain_existing_path("../x"), workflowr:::absolute(".."))
+  expect_identical(obtain_existing_path("../x/"), workflowr:::absolute(".."))
+  expect_identical(obtain_existing_path("../x/y"), workflowr:::absolute(".."))
+  expect_identical(obtain_existing_path("../x/y/"), workflowr:::absolute(".."))
+  expect_identical(obtain_existing_path("../x/y/z"), workflowr:::absolute(".."))
 })
 
 test_that("obtain_existing_paths works with relative path to two upstream", {
-expect_identical(obtain_existing_path("../../x"), workflowr:::absolute("../.."))
-expect_identical(obtain_existing_path("../../x/"), workflowr:::absolute("../.."))
-expect_identical(obtain_existing_path("../../x/y"), workflowr:::absolute("../.."))
-expect_identical(obtain_existing_path("../../x/y/"), workflowr:::absolute("../.."))
-expect_identical(obtain_existing_path("../../x/y/z"), workflowr:::absolute("../.."))
+  expect_identical(obtain_existing_path("../../x"), workflowr:::absolute("../.."))
+  expect_identical(obtain_existing_path("../../x/"), workflowr:::absolute("../.."))
+  expect_identical(obtain_existing_path("../../x/y"), workflowr:::absolute("../.."))
+  expect_identical(obtain_existing_path("../../x/y/"), workflowr:::absolute("../.."))
+  expect_identical(obtain_existing_path("../../x/y/z"), workflowr:::absolute("../.."))
 })
 
 test_that("obtain_existing_paths works with relative path to home directory", {
-expect_identical(obtain_existing_path("~/x"), workflowr:::absolute("~"))
-expect_identical(obtain_existing_path("~/x/"), workflowr:::absolute("~"))
-expect_identical(obtain_existing_path("~/x/y"), workflowr:::absolute("~"))
-expect_identical(obtain_existing_path("~/x/y/"), workflowr:::absolute("~"))
-expect_identical(obtain_existing_path("~/x/y/z"), workflowr:::absolute("~"))
+  expect_identical(obtain_existing_path("~/x"), workflowr:::absolute("~"))
+  expect_identical(obtain_existing_path("~/x/"), workflowr:::absolute("~"))
+  expect_identical(obtain_existing_path("~/x/y"), workflowr:::absolute("~"))
+  expect_identical(obtain_existing_path("~/x/y/"), workflowr:::absolute("~"))
+  expect_identical(obtain_existing_path("~/x/y/z"), workflowr:::absolute("~"))
 })
 
-test_that("obtain_existing_paths works when the root is the upstream existing directory", {
+test_that("obtain_existing_paths works when the root is the upstream existing directory (unix-like)", {
 
-  skip_on_cran()
-  # These only fail on winbuilder for some reason
-  #
-  # -- 1. Failure: obtain_existing_paths works when the root is the upstream existin
-  # obtain_existing_path("/x") not identical to workflowr:::absolute("/").
-  # 1/1 mismatches
-  # x[1]: "D:/x"
-  # y[1]: "D:/"
-  #
-  # -- 2. Failure: obtain_existing_paths works when the root is the upstream existin
-  # obtain_existing_path("/x/") not identical to workflowr:::absolute("/").
-  # 1/1 mismatches
-  # x[1]: "D:/x"
-  # y[1]: "D:/"
-  #
-  # -- 3. Failure: obtain_existing_paths works when the root is the upstream existin
-  # obtain_existing_path("/x/y") not identical to workflowr:::absolute("/").
-  # 1/1 mismatches
-  # x[1]: "D:/x/y"
-  # y[1]: "D:/"
-  #
-  # -- 4. Failure: obtain_existing_paths works when the root is the upstream existin
-  # obtain_existing_path("/x/y/") not identical to workflowr:::absolute("/").
-  # 1/1 mismatches
-  # x[1]: "D:/x/y"
-  # y[1]: "D:/"
-  #
-  # -- 5. Failure: obtain_existing_paths works when the root is the upstream existin
-  # obtain_existing_path("/x/y/z") not identical to workflowr:::absolute("/").
-  # 1/1 mismatches
-  # x[1]: "D:/x/y/z"
-  # y[1]: "D:/"
+  if (.Platform$OS.type == "windows") skip("Only relevant on Unix-like")
 
   expect_identical(obtain_existing_path("/x"), workflowr:::absolute("/"))
   expect_identical(obtain_existing_path("/x/"), workflowr:::absolute("/"))
   expect_identical(obtain_existing_path("/x/y"), workflowr:::absolute("/"))
   expect_identical(obtain_existing_path("/x/y/"), workflowr:::absolute("/"))
   expect_identical(obtain_existing_path("/x/y/z"), workflowr:::absolute("/"))
+})
+
+test_that("obtain_existing_paths works when the root is the upstream existing directory (windows)", {
+
+  if (.Platform$OS.type != "windows") skip("Only relevant on Windows")
+
+  expect_identical(obtain_existing_path("C:/x"), workflowr:::absolute("C:/"))
+  expect_identical(obtain_existing_path("C:/x/"), workflowr:::absolute("C:/"))
+  expect_identical(obtain_existing_path("C:/x/y"), workflowr:::absolute("C:/"))
+  expect_identical(obtain_existing_path("C:/x/y/"), workflowr:::absolute("C:/"))
+  expect_identical(obtain_existing_path("C:/x/y/z"), workflowr:::absolute("C:/"))
 })

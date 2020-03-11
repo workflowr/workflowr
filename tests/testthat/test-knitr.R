426ef2cf4c2c016df79de58699e17a8cdd8fc040
John Blischak <jdblischak@gmail.com>
2020-03-11 08:35:50 -0400

    Fix test of knitr::include_graphics().
    The argument `error` doesn't exist in knitr 1.27 and earlier. This was
    caught by workflowr-pkg-tests.
    
    https://github.com/workflowr/workflowr-pkg-tests
    https://circleci.com/gh/workflowr/workflowr-pkg-tests/516

--- tests/testthat/test-knitr.R
+++ tests/testthat/test-knitr.R
@@ -6,12 +6,21 @@ test_that("include_graphics() can create HTML tag for file it can't find", {
   # This is critical because the Rmd files are in analysis/, code chunks
   # executed in the project root (by default), and HTML files saved in docs/. In
   # order to reference image files relative to docs/, need to be able to create
-  # the links even if knitr can't currently identify the files
+  # the links even if knitr can't currently identify the files. The default
+  # behavior changed in knitr 1.28.
   # https://github.com/yihui/knitr/issues/1717#issuecomment-583373829
   # https://jdblischak.github.io/workflowr/articles/wflow-05-faq.html#how-can-i-include-external-images-in-my-website
 
-  expect_silent(
-    result <- knitr::include_graphics("docs/assets/external.png", error = FALSE)
-  )
+  version_knitr <- utils::packageVersion("knitr")
+
+  if(version_knitr >= "1.28") {
+    expect_silent(
+      result <- knitr::include_graphics("docs/assets/external.png", error = FALSE)
+    )
+  } else {
+    expect_silent(
+      result <- knitr::include_graphics("docs/assets/external.png")
+    )
+  }
   expect_identical(as.character(result), "docs/assets/external.png")
 })

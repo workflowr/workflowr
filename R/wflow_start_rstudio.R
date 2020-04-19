c2d7cbaa43f45132a9f3b30433d66feecee87f8c
Anh N Tran <trannhatanh89@gmail.com>
2020-04-19 10:31:08 -0500

    move the new workflowr template out of beta
    - remove change_wd, disable_remote, and dry_run from the dcf file and wflow_start_rstudio()
    - overwrite the original dcf file

--- R/wflow_start_rstudio.R
+++ R/wflow_start_rstudio.R
@@ -1,15 +1,13 @@
+## Wrapper function to enhance RStudio Project Template
+
 wflow_start_rstudio <- function(directory,
                                 name = NULL,
                                 git = TRUE,
                                 existing = FALSE,
                                 overwrite = FALSE,
-                                change_wd = TRUE,
-                                disable_remote = FALSE,
-                                dry_run = FALSE,
                                 user.name = NULL,
                                 user.email = NULL) {
 
-  #
   directory_rs <- directory
 
   # Check if name is blank, use NULL if true
@@ -24,12 +22,6 @@ wflow_start_rstudio <- function(directory,
   existing_rs <- existing
 
   overwrite_rs <- overwrite
-
-  change_wd_rs <- change_wd
-
-  disable_remote_rs <- disable_remote
-
-  dry_run_rs <- dry_run
 
   # Check if user.name is blank, use NULL if true
   if (user.name == "") {
@@ -47,26 +39,11 @@ wflow_start_rstudio <- function(directory,
     user.email_rs <- user.email
   }
 
-  ## dev: print out the checked variable
-  # print(directory_rs)
-  # print(name_rs)
-  # print(git_rs)
-  # print(existing_rs)
-  # print(overwrite_rs)
-  # print(change_wd_rs)
-  # print(disable_remote_rs)
-  # print(dry_run_rs)
-  # print(user.name_rs)
-  # print(user.email_rs)
-
   wflow_start(directory = directory_rs,
               name = name_rs,
               git = git_rs,
               existing = existing_rs,
               overwrite = overwrite_rs,
-              change_wd = change_wd_rs,
-              disable_remote = disable_remote_rs,
-              dry_run = dry_run_rs,
               user.name = user.name_rs,
               user.email = user.email_rs)
 }

f42201e82503ab1a0fac294989de387b83ea5661
Anh N Tran <trannhatanh89@gmail.com>
2020-04-28 23:26:51 -0500

    Try fixing wflow_start_rstudio test using on.exit
      site_dir <- tempfile()
      cwd <- getwd()
      on.exit(setwd(cwd))
      on.exit(unlink(site_dir, recursive = TRUE, force = TRUE), add = TRUE)

--- tests/testthat/test-wflow_start_rstudio.R
+++ tests/testthat/test-wflow_start_rstudio.R
@@ -21,6 +21,9 @@ test_that("wflow_start_studio accepts custom name", {
 
   project_name <- "A new project"
   site_dir <- tempfile()
+  cwd <- getwd()
+  on.exit(setwd(cwd))
+  on.exit(unlink(site_dir, recursive = TRUE, force = TRUE), add = TRUE)
   utils::capture.output(wflow_start_rstudio(directory = site_dir,
                                             name = project_name,
                                             user.name = "Test Name",
@@ -38,96 +41,126 @@ test_that("wflow_start_studio accepts custom name", {
 
 
 
-# test_that("wflow_start_rstudio initializes Git repository by default", {
+test_that("wflow_start_rstudio initializes Git repository by default", {
+
+  # start project in a tempdir
+  site_dir <- tempfile()
+  cwd <- getwd()
+  on.exit(setwd(cwd))
+  on.exit(unlink(site_dir, recursive = TRUE, force = TRUE), add = TRUE)
+  utils::capture.output(wflow_start_rstudio(site_dir,
+                                            user.name = "Test Name",
+                                            user.email = "test@email"))
+  site_dir <- workflowr:::absolute(site_dir)
+  expect_true(git2r::in_repository(site_dir))
+  unlink(site_dir, recursive = TRUE, force = TRUE)
+})
+
+
+test_that("wflow_start_rstudio git = FALSE does not initialize a Git repository", {
+
+  # start project in a tempdir
+  site_dir <- tempfile()
+  cwd <- getwd()
+  on.exit(setwd(cwd))
+  on.exit(unlink(site_dir, recursive = TRUE, force = TRUE), add = TRUE)
+  o <- wflow_start_rstudio(site_dir,
+                           git = FALSE,
+                           user.name = "Test Name",
+                           user.email = "test@email")
+  site_dir <- workflowr:::absolute(site_dir)
+  expect_false(git2r::in_repository(site_dir))
+  expect_null(o$commit)
+  unlink(site_dir, recursive = TRUE, force = TRUE)
+})
+
+test_that("wflow_start_rstudio does not overwrite files by default", {
+
+  # start project in a tempdir
+  site_dir <- tempfile()
+  cwd <- getwd()
+  on.exit(setwd(cwd))
+  on.exit(unlink(site_dir, recursive = TRUE, force = TRUE), add = TRUE)
+  fs::dir_create(site_dir)
+  site_dir <- workflowr:::absolute(site_dir)
+  readme_file <- file.path(site_dir, "README.md")
+  writeLines("original", con = readme_file)
+  rprofile_file <- file.path(site_dir, ".Rprofile")
+  writeLines("x <- 1", con = rprofile_file)
+  utils::capture.output(wflow_start_rstudio(site_dir,
+                                            existing = TRUE,
+                                            user.name = "Test Name",
+                                            user.email = "test@email"))
+
+  readme_contents <- readLines(readme_file)
+  expect_true(readme_contents == "original")
+  rprofile_contents <- readLines(rprofile_file)
+  expect_true(rprofile_contents == "x <- 1")
+  unlink(site_dir, recursive = TRUE, force = TRUE)
+})
+
+
+test_that("wflow_start_rstudio overwrites files when forced", {
+
+  # start project in a tempdir
+  site_dir <- tempfile()
+  cwd <- getwd()
+  on.exit(setwd(cwd))
+  on.exit(unlink(site_dir, recursive = TRUE, force = TRUE), add = TRUE)
+  fs::dir_create(site_dir)
+  site_dir <- workflowr:::absolute(site_dir)
+  readme_file <- file.path(site_dir, "README.md")
+  writeLines("original", con = readme_file)
+  rprofile_file <- file.path(site_dir, ".Rprofile")
+  writeLines("x <- 1", con = rprofile_file)
+  utils::capture.output(wflow_start_rstudio(site_dir,
+                                            existing = TRUE,
+                                            overwrite = TRUE,
+                                            user.name = "Test Name",
+                                            user.email = "test@email"))
+
+  readme_contents <- readLines(readme_file)
+  expect_true(readme_contents[1] == sprintf("# %s", basename(site_dir)))
+  rprofile_contents <- readLines(rprofile_file)
+  expect_false(any(rprofile_contents == "x <- 1"))
+  unlink(site_dir, recursive = TRUE, force = TRUE)
+})
+
+
+test_that("wflow_start_rstudio can handle deeply nested paths that need to be created", {
+
+  # Create and move to a temp directory
+  site_dir <- tempfile("test-deeply-nested-")
+  fs::dir_create(site_dir)
+  site_dir <- workflowr:::absolute(site_dir)
+  cwd <- getwd()
+  setwd(site_dir)
+  on.exit(setwd(cwd))
+  on.exit(unlink(site_dir, recursive = TRUE, force = TRUE), add = TRUE)
+
+  dir_test <- "a/b/c/x/y/z"
+  expected <- file.path(workflowr:::absolute("."), dir_test)
+  utils::capture.output(actual <- wflow_start_rstudio(dir_test,
+                                                      user.name = "Test Name",
+                                                      user.email = "test@email"))
+  expect_identical(actual$directory, expected)
+  expect_true(fs::file_exists(file.path(expected, "z.Rproj")))
+})
+
+# test_that("wflow_start_rstudio throws an error if user.name and user.email are not set", {
 #
-#   # start project in a tempdir
+#   skip_on_cran()
+#
+#   # local_no_gitconfig() is defined in tests/testthat/helpers.R
+#   local_no_gitconfig("-workflowr")
+#
 #   site_dir <- tempfile()
-#   utils::capture.output(wflow_start_rstudio(site_dir,
-#                                             user.name = "Test Name",
-#                                             user.email = "test@email"))
-#   site_dir <- workflowr:::absolute(site_dir)
-#   expect_true(git2r::in_repository(site_dir))
-#   unlink(site_dir, recursive = TRUE, force = TRUE)
-# })
-#
-#
-# test_that("wflow_start_rstudio git = FALSE does not initialize a Git repository", {
-#
-#   # start project in a tempdir
-#   site_dir <- tempfile()
-#   o <- wflow_start_rstudio(site_dir,
-#                            git = FALSE,
-#                            user.name = "Test Name",
-#                            user.email = "test@email")
-#   site_dir <- workflowr:::absolute(site_dir)
-#   expect_false(git2r::in_repository(site_dir))
-#   expect_null(o$commit)
-#   unlink(site_dir, recursive = TRUE, force = TRUE)
-# })
-#
-# test_that("wflow_start_rstudio does not overwrite files by default", {
-#
-#   # start project in a tempdir
-#   site_dir <- tempfile()
-#   fs::dir_create(site_dir)
-#   site_dir <- workflowr:::absolute(site_dir)
-#   readme_file <- file.path(site_dir, "README.md")
-#   writeLines("original", con = readme_file)
-#   rprofile_file <- file.path(site_dir, ".Rprofile")
-#   writeLines("x <- 1", con = rprofile_file)
-#   utils::capture.output(wflow_start_rstudio(site_dir,
-#                                             existing = TRUE,
-#                                             user.name = "Test Name",
-#                                             user.email = "test@email"))
-#
-#   readme_contents <- readLines(readme_file)
-#   expect_true(readme_contents == "original")
-#   rprofile_contents <- readLines(rprofile_file)
-#   expect_true(rprofile_contents == "x <- 1")
-#   unlink(site_dir, recursive = TRUE, force = TRUE)
-# })
-#
-#
-# test_that("wflow_start_rstudio overwrites files when forced", {
-#
-#   # start project in a tempdir
-#   site_dir <- tempfile()
-#   fs::dir_create(site_dir)
-#   site_dir <- workflowr:::absolute(site_dir)
-#   readme_file <- file.path(site_dir, "README.md")
-#   writeLines("original", con = readme_file)
-#   rprofile_file <- file.path(site_dir, ".Rprofile")
-#   writeLines("x <- 1", con = rprofile_file)
-#   utils::capture.output(wflow_start_rstudio(site_dir,
-#                                             existing = TRUE,
-#                                             overwrite = TRUE,
-#                                             user.name = "Test Name",
-#                                             user.email = "test@email"))
-#
-#   readme_contents <- readLines(readme_file)
-#   expect_true(readme_contents[1] == sprintf("# %s", basename(site_dir)))
-#   rprofile_contents <- readLines(rprofile_file)
-#   expect_false(any(rprofile_contents == "x <- 1"))
-#   unlink(site_dir, recursive = TRUE, force = TRUE)
-# })
-#
-#
-# test_that("wflow_start_rstudio can handle deeply nested paths that need to be created", {
-#
-#   # Create and move to a temp directory
-#   site_dir <- tempfile("test-deeply-nested-")
-#   fs::dir_create(site_dir)
-#   site_dir <- workflowr:::absolute(site_dir)
 #   cwd <- getwd()
-#   setwd(site_dir)
 #   on.exit(setwd(cwd))
 #   on.exit(unlink(site_dir, recursive = TRUE, force = TRUE), add = TRUE)
-#
-#   dir_test <- "a/b/c/x/y/z"
-#   expected <- file.path(workflowr:::absolute("."), dir_test)
-#   utils::capture.output(actual <- wflow_start_rstudio(dir_test,
-#                                                       user.name = "Test Name",
-#                                                       user.email = "test@email"))
-#   expect_identical(actual$directory, expected)
-#   expect_true(fs::file_exists(file.path(expected, "z.Rproj")))
+#   expect_error(wflow_start_rstudio(site_dir),
+#                "You must set your user.name and user.email for Git first")
+#   expect_false(fs::dir_exists(site_dir))
+#   expect_error(wflow_start_rstudio(site_dir),
+#                "the RStudio Project Template")
 # })
